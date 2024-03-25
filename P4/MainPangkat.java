13f9e7999eb658ef19cfa856fc07787d4a94cfd2
Rakai Seto <mamangrakai@gmail.com>
2024-03-25 13:33:37 +0700

    P4_Percobaan_Praktikum_2_Pertanyaan_4

--- P4/MainPangkat.java
+++ P4/MainPangkat.java
@@ -17,13 +17,36 @@ public class MainPangkat {
             pk[i] = new Pangkat(nilai, pangkat);
         }
 
-        System.out.println("Hasil Pangkat dengan Brute Force");
-        for (int i = 0; i < iJml; i++) {
-            System.out.println("Pangkat dari " + pk[i].nilai + " pangkat " + pk[i].pangkat + " adalah: " + pk[i].hitungPangkatBF());
-        }
-        System.out.println("Hasil Pangkat dengan Divide and Conquer");
-        for (int i = 0; i < iJml; i++) {
-            System.out.println("Pangkat dari " + pk[i].nilai + " pangkat " + pk[i].pangkat + " adalah: " + pk[i].hitungPangkatDC(pk[i].nilai, pk[i].pangkat));
+        boolean isTrue = false;
+        while (!isTrue) {
+//            get menu
+            System.out.println("Pilih menu: ");
+            System.out.println("1. Hitung Pangkat dengan Brute Force");
+            System.out.println("2. Hitung Pangkat dengan Divide and Conquer");
+            System.out.println("3. Keluar");
+            int menu = sc26.nextInt();
+            switch (menu) {
+                case 1:
+                    System.out.println("Hasil Pangkat dengan Brute Force");
+                    for (int i = 0; i < iJml; i++) {
+                        System.out.println("Pangkat dari " + pk[i].nilai + " pangkat " + pk[i].pangkat + " adalah: " + pk[i].hitungPangkatBF());
+                    }
+                    isTrue = true;
+                    break;
+                case 2:
+                    System.out.println("Hasil Pangkat dengan Divide and Conquer");
+                    for (int i = 0; i < iJml; i++) {
+                        System.out.println("Pangkat dari " + pk[i].nilai + " pangkat " + pk[i].pangkat + " adalah: " + pk[i].hitungPangkatDC(pk[i].nilai, pk[i].pangkat));
+                    }
+                    isTrue = true;
+                    break;
+                case 3:
+                    isTrue = true;
+                    break;
+                default:
+                    System.out.println("Menu tidak tersedia");
+                    break;
+            }
         }
     }
 }
