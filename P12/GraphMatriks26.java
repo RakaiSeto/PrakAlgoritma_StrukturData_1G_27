e308510e90d0b22dc8ba24e3df96ee30f6e47fc2
Rakai Seto <mamangrakai@gmail.com>
2024-06-07 19:28:33 +0700

    P12_Percobaan_2_26

---
+++ P12/GraphMatriks26.java
@@ -0,0 +1,33 @@
+package P12;
+
+public class GraphMatriks26 {
+    int vertex;
+    int[][] matriks;
+
+    public GraphMatriks26(int vertex) {
+        this.vertex = vertex;
+        matriks = new int[vertex][vertex];
+    }
+
+    public void makeEdge(int asal, int tujuan, int jarak) {
+        matriks[asal][tujuan] = jarak;
+    }
+
+    public void removeEdge(int asal, int tujuan) {
+        matriks[asal][tujuan] = 0;
+    }
+
+    public void printGraph() {
+        for (int i = 0; i < vertex; i++) {
+            System.out.println("Gedung " + (char) ('A' + i) + ": ");
+            for (int j = 0; j < vertex; j++) {
+                if (matriks[i][j] != -1) {
+                    System.out.print("Gedung " + (char) ('A' + j) + " (" + matriks[i][j] + " m), ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+
+}
